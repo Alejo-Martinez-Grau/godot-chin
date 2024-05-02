785f74cfbc607a9fa2e9dd7efeb1429cb260105d
Alejo <alejomartinezgrau@gmail.com>
2024-05-02 11:39:16 -0300

    outline for playing card WIP

--- main.gd
+++ main.gd
@@ -20,32 +20,30 @@ func init():
 	var deck = makeDeck()
 	playerDeck = deck.slice(0,26,1)
 	oponentDeck = deck.slice(26,52,1)
-	print("deck", deck)
-	print("playerDeck ", playerDeck.size(), playerDeck[0], playerDeck[-1])
-	print("oponentDeck ", oponentDeck.size(), oponentDeck[0], oponentDeck[-1])
+	#print("deck", deck)
+	#print("playerDeck ", playerDeck.size(), playerDeck[0], playerDeck[-1])
+	#print("oponentDeck ", oponentDeck.size(), oponentDeck[0], oponentDeck[-1])
 	
 	for i in range(1, 6):
 		get_node("Card" + str(i)).setValue(playerDeck.pop_front())
 		get_node("Card" + str(i)).visible = true
+		
 	for i in range(6, 11):
 		get_node("Card" + str(i)).setValue(oponentDeck.pop_front())
 		get_node("Card" + str(i)).visible = true
-	
-	print("playerDeck ", playerDeck.size(), playerDeck[0], playerDeck[-1])
-	print("oponentDeck ", oponentDeck.size(), oponentDeck[0], oponentDeck[-1])
+	#
+	#print("playerDeck ", playerDeck.size(), playerDeck[0], playerDeck[-1])
+	#print("oponentDeck ", oponentDeck.size(), oponentDeck[0], oponentDeck[-1])
 
 func _process(_delta):
 	if(Input.is_action_pressed("selectCard1") && $Card1.visible):
-		print("card1")
+		#get_node("Card1/Panel").visible = true
 		setCard($Card1)
 	if(Input.is_action_pressed("selectCard2") && $Card2.visible):
-		print("card2")
 		setCard($Card2)
 	if(Input.is_action_pressed("selectCard3") && $Card3.visible):
-		print("card3")
 		setCard($Card3)
 	if(Input.is_action_pressed("selectCard4") && $Card4.visible):
-		print("card4")
 		setCard($Card4)
 
 func setCard(cardNode: Node):
